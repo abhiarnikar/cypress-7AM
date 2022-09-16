2c8134426f2f3124af2ae699dfd3f3965cbfba3e
abhiarnikar <abhiarnikar725@gmail.com>
2022-09-16 18:05:58 +0530

    API

---
+++ cypress/e2e/API/API.cy.js
@@ -0,0 +1,47 @@
+/// <reference types="cypress" />
+
+describe('validate the API',()=>{
+
+    it('validate the GET API',function(){
+        cy.request({
+            method:"GET",
+            url:"https://reqres.in/api/users?page=2"
+        }).then(function(res){
+            cy.log(res)
+            expect(res.status).to.eq(200)
+        })
+    })
+    it('validate the POST API',()=>{
+        cy.request({
+            method:"POST",
+            url:"https://reqres.in/api/users",
+            body:{
+                "name": "morpheus",
+                "job": "leader"
+            }
+        }).then((res)=>{
+            expect(res.status).to.eq(201)
+        })
+    })
+    it('validate the PUT API',()=>{
+        cy.request({
+            method:"PUT",
+            url:"https://reqres.in/api/users/2",
+            body:{
+                "name": "morpheus",
+                "job": "zion resident"
+            }
+        }).then((res)=>{
+            expect(res.status).to.eq(200)
+        })
+    })
+    it('validate the DELETE API',()=>{
+        cy.request({
+            method:"DELETE",
+            url:"https://reqres.in/api/users/2"
+        }).then((res)=>{
+            expect(res.status).to.eq(204)
+        })
+    })
+
+})
